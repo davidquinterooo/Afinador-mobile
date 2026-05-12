2675792fec6388e6709b19b7aa0b8afb5df1b271
David <xdavidsimon@gmail.com>
2026-05-12 12:22:35 -0400

    Agregados capacitor y android al afinador

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.afinadorMovile.app',
+  appName: 'com.afinador-mobile.app',
+  webDir: '.output/public'
+};
+
+export default config;
