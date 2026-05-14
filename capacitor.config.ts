f6f792bd6f8e50b2cc8f9e798cab93782324b300
The Exar <chapasito13@gmail.com>
2026-05-14 10:24:49 -0400

    Se agrego que aceptara buscar rutas con http, se cambio el nombre de la aplicacion a A-CORD-E, y se modifico la version para dejar de ser la 1.0 a 1.0.1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.afinadorMovile.app',
   appName: 'com.afinador-mobile.app',
-  webDir: '.output/public'
+  webDir: '.output/public',
+  server: {
+    androidScheme: 'http'
+  }
 };
 
 export default config;
