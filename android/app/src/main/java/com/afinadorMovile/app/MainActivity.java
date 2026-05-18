ad3cd8e048aa0de9e0e83202f678208792614c24
The Exar <chapasito13@gmail.com>
2026-05-18 12:32:10 -0400

    Se realizo una anulacion selectiva de seguridad en el entorno de android del WebView para que sea modo mixto y lea completamente el http y no busque en otros puertos o direcciones

--- android/app/src/main/java/com/afinadorMovile/app/MainActivity.java
+++ android/app/src/main/java/com/afinadorMovile/app/MainActivity.java
@@ -1,5 +1,24 @@
 package com.afinadorMovile.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Obtener el WebView de Capacitor y permitir contenido mixto
+        WebView webView = this.bridge.getWebView();
+        if (webView != null) {
+            WebSettings settings = webView.getSettings();
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+    }
+}
